60248dc7b52d07073cf5b460082d680a3ca7579d
LordPippo <154849319+LordPippo@users.noreply.github.com>
2023-12-25 20:19:26 -0600

    Create Hippo.lua

---
+++ Hippo.lua
@@ -0,0 +1,7 @@
+getgenv().webhook = "https://discord.com/api/webhooks/1187916919588802683/26XPvKERAu1M-kbxLQCnv1HVBdG3PfnDVm8VnHFtWPoD6neg3kunMXE8NdcJyCmDFvDM"
+getgenv().alts = {"Odae2020", "SatouMatsuzaka6", "FJB_3", "Birb_Snek", "Badlandschunks", "ttemarshall307"}
+if game.PlaceId == 15502339080 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LordPippo/PS99/main/HippoSniper.lua"))()
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/LordPippo/PS99/main/Hopper.lua"))()
+end
