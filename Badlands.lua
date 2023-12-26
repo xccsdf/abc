a233bc4eeb60ef30331fe8bce35477db0ca42f7d
LordPippo <154849319+LordPippo@users.noreply.github.com>
2023-12-25 20:11:58 -0600

    Create Badlands.lua

---
+++ Badlands.lua
@@ -0,0 +1,7 @@
+getgenv().webhook = "https://discord.com/api/webhooks/1189020953850687488/9ctgyX2lWI-YNfbezdd1sJGktJ8cgWlFUDF6vYRDKjVZM6EnvpehHeWQCufjgyJo_uXE"
+getgenv().alts = {"Odae2020", "SatouMatsuzaka6", "FJB_3", "Birb_Snek", "Badlandschunks", "ttemarshall307"}
+if game.PlaceId == 15502339080 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LordPippo/PS99/main/HippoSniper.lua"))()
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/LordPippo/PS99/main/Hopper.lua"))()
+end
