775ed25f751f4e3bfaab8508b6cd08163032f307
LordPippo <154849319+LordPippo@users.noreply.github.com>
2023-12-25 19:13:56 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -1,7 +1,13 @@
+repeat wait() until game:IsLoaded()
+
 setfpscap(5)
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
-local Library = require(game.ReplicatedStorage:WaitForChild('Library'))
+local message1 = {}
+local Players = game:GetService('Players')
+local PlayerInServer = #Players:GetPlayers()
+local ostime = os.time()
+
 
 if not getgenv().a then
     getgenv().a = true
@@ -11,6 +17,7 @@ if not getgenv().a then
         wait(1)
         vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
     end)
+    
 end
 
 local function processListingInfo(uid, gems, item, version, shiny, amount, boughtFrom)
@@ -46,74 +53,73 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
         amount = 1
         print("AMOUNT:", amount)
     end
-
-    local fields = {
-        {
-            name = "PRICE:",
-            value = tostring(gems) .. " GEMS",
-            inline = true,
-        },
-        {
-            name = "BOUGHT FROM:",
-            value = tostring(boughtFrom),
-            inline = true,
-        },
-        {
-            name = "AMOUNT:",
-            value = tostring(amount),
-            inline = true,
-        },
-        {
-            name = "PETID:",
-            value = tostring(uid),
-            inline = true,
+    
+    message1 = {
+        ['content'] = "LordHippos Sniper",
+        ['embeds'] = {
+            {
+                ['title'] = snipeMessage,
+                ["color"] = tonumber(0x33dd99),
+                ["timestamp"] = DateTime.now():ToIsoDate(),
+                ['fields'] = {
+                    {
+                        ['name'] = "PRICE:",
+                        ['value'] = tostring(gems) .. " GEMS",
+                    },
+                    {
+                        ['name'] = "BOUGHT FROM:",
+                        ['value'] = tostring(boughtFrom),
+                    },
+                    {
+                        ['name'] = "AMOUNT:",
+                        ['value'] = tostring(amount),
+                    },
+                    {
+                        ['name'] = "PETID:",
+                        ['value'] = tostring(uid),
+                    },
+                },
+            },
         }
     }
 
-    local message = {
-        content = "@everyone",
-        embeds = {
-            {
-                title = snipeMessage,
-                fields = fields,
-                author = {name = "New Pet Sniped!"}
-            }
-        },
-        username = "piratesniper",
-        attachments = {}
-    }
-
     local http = game:GetService("HttpService")
-    local jsonMessage = http:JSONEncode(message)
-
-    http:PostAsync(
-        "https://discord.com/api/webhooks/1187916919588802683/26XPvKERAu1M-kbxLQCnv1HVBdG3PfnDVm8VnHFtWPoD6neg3kunMXE8NdcJyCmDFvDM",
-        jsonMessage,
-        Enum.HttpContentType.ApplicationJson,
-        false
-    )
+    local jsonMessage = http:JSONEncode(message1)
+    http:PostAsync(getgenv().webhook, jsonMessage)
 end
 
 local function checklisting(uid, gems, item, version, shiny, amount, username, playerid)
+    local Library = require(game.ReplicatedStorage:WaitForChild('Library'))
     gems = tonumber(gems)
-
     local type = {}
     pcall(function()
-      type = Library.Directory.Pets[item]
-    end)
-    
-    if type.exclusiveLevel and gems <= 10000 then
-    game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        type = Library.Directory.Pets[item]
+end)
+
+    if type.exclusiveLevel and gems <= 10000 and item ~= "Banana" and item ~= "Coin" then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif item == "Titanic Christmas Present" and gems <= 10000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif string.find(item, "Exclusive") and gems <= 10000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif string.find(item, "Charm") and gems <= 1000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username)
     elseif type.huge and gems <= 1000000 then
-    game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username)     
     elseif type.titanic and gems <= 10000000 then
-    game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username)      
     end
 end
 
 Booths_Broadcast.OnClientEvent:Connect(function(username, message)
-    if type(message) == "table" and message['PlayerID'] then
-        local playerID = message['PlayerID']
+    local playerID = message['PlayerID']
+    if type(message) == "table" then
         local listing = message["Listings"]
         for key, value in pairs(listing) do
             if type(value) == "table" then
@@ -136,3 +142,44 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
         end
     end
 end)
+
+local function jumpToServer() 
+    local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true" 
+    local req = request({ Url = string.format(sfUrl, 15502339080, "Desc", 100) }) 
+    local body = game:GetService("HttpService"):JSONDecode(req.Body) 
+    local deep = math.random(1, 3)
+    if deep > 1 then 
+        for i = 1, deep, 1 do 
+            req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 15502339080, "Desc", 100) }) 
+            body = game:GetService("HttpService"):JSONDecode(req.Body) 
+            task.wait(0.1)
+        end 
+    end 
+    local servers = {} 
+    if body and body.data then 
+        for i, v in next, body.data do 
+            if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+                table.insert(servers, 1, v.id)
+            end
+        end
+    end
+    local randomCount = #servers
+    if not randomCount then
+        randomCount = 2
+    end
+    game:GetService("TeleportService"):TeleportToPlaceInstance(15502339080, servers[math.random(1, randomCount)], game:GetService("Players").LocalPlayer) 
+end
+
+while wait(0.1) do
+    PlayerInServer = #Players:GetPlayers()
+    if PlayerInServer < 25 or os.time() >= ostime + 900 then
+        jumpToServer()
+        break
+    end
+    for count = 1, #alts, 1 do
+        if game.Players:FindFirstChild(alts[count]) and alts[count] ~= game:GetService("Players").LocalPlayer.Name then
+            jumpToServer()
+            break
+        end
+    end
+end 
