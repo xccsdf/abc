03b12d6975be9bb4fb27f5eba1ca62eb72f8c7d6
LordPippo <154849319+LordPippo@users.noreply.github.com>
2023-12-27 19:27:16 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -1,6 +1,6 @@
 repeat wait() until game:IsLoaded()
 
-setfpscap(10)
+setfpscap(5)
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
 local message1 = {}
