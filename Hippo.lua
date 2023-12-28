cf380176c48fe879c850f6aaf099808b0e2155fb
LordPippo <154849319+LordPippo@users.noreply.github.com>
2023-12-28 13:57:58 -0600

    Update Hippo.lua

--- Hippo.lua
+++ Hippo.lua
@@ -1,5 +1,5 @@
-getgenv().webhook = ""
-getgenv().alts = {}
+getgenv().webhook = "https://discord.com/api/webhooks/1187916919588802683/26XPvKERAu1M-kbxLQCnv1HVBdG3PfnDVm8VnHFtWPoD6neg3kunMXE8NdcJyCmDFvDM"
+getgenv().alts = {"Odae2020", "SatouMatsuzaka6", "FJB_3", "Birb_Snek", "Badlandschunks", "ttemarshall307"}
 if game.PlaceId == 15502339080 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/LordPippo/PS99/main/HippoSniper.lua"))()
 else
