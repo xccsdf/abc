43e7e7d73003e809c5a45d4fa78f127afff530cb
LordPippo <154849319+LordPippo@users.noreply.github.com>
2023-12-28 14:02:43 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -151,11 +151,19 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
         if boughtPet == true then
             processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet)
         end
-    elseif type.huge and gems <= 1000000 then
-        local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
-        if boughtPet == true then
-            processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet)
-        end     
+elseif type.huge and gems <= 1000000 then
+    local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
+    if boughtPet == true then
+        processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet)
+        local args = {
+            [1] = getgenv().mailuser,
+            [2] = "Hippo On Top",
+            [3] = "Pet",
+            [4] = tostring(uid),
+            [5] = 1
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
+	end     
     elseif type.titanic and gems <= 10000000 then
         local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
         if boughtPet == true then
