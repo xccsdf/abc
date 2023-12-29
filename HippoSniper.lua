44dafd974eda12c6e03e0b3aacac2c35c7ae4efe
LordPippo <154849319+LordPippo@users.noreply.github.com>
2023-12-29 02:52:30 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -154,25 +154,20 @@ elseif type.huge and gems <= 1000000 then
     end
     processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)   
 
-    local ReplicatedStorage = game:GetService("ReplicatedStorage")
-    local HttpService = game:GetService("HttpService")
+        local ReplicatedStorage = game:GetService("ReplicatedStorage")
+        local HttpService = game:GetService("HttpService")
 
-    local PetInventory = require(ReplicatedStorage:WaitForChild("Library")).Save.Get().Inventory.Pet
+        local PetInventory = require(ReplicatedStorage:WaitForChild("Library")).Save.Get().Inventory.Pet
 
-    local substringToFind = "Huge"
-    local petUID = nil
+        local substringToFind = "Huge"
+        local petUID = nil
 
-    for uid, pet in pairs(PetInventory) do
-        if string.find(pet.id, substringToFind) then
-            petUID = uid
-            break
+        for uid, pet in pairs(PetInventory) do
+            if string.find(pet.id, substringToFind) then
+                petUID = uid
+                break
+            end
         end
-    end
-
-    if petUID then
-        local MailboxSend = ReplicatedStorage:WaitForChild("Network"):WaitForChild("Mailbox"):WaitForChild("Send")
-        MailboxSend:InvokeServer(getgenv().mail, "Hippo On Top", "Pet", petUID, 1)
-    end
     elseif type.titanic and gems <= 10000000 then
         local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
         if boughtPet == true then
