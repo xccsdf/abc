24aad081e97057fda64fad58fd36f49bd7bfc661
LordPippo <154849319+LordPippo@users.noreply.github.com>
2023-12-29 23:40:36 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -72,6 +72,10 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
         ['content'] = webContent,
         ['embeds'] = {
             {
+		["author"] = {
+			["name"] = "Reimu 🤑",
+			["icon_url"] = "https://cdn.discordapp.com/attachments/1122535236996182099/1189213923073871953/EmrJ9tNVcAIhVzB.png?ex=659d58c5&is=658ae3c5&hm=c55bc9b5323c6aa542d6a99b4e42c20a0255377566c3bc2d047f63bffce70b7e&",
+		},
                 ['title'] = snipeMessage,
                 ["color"] = webcolor,
                 ["timestamp"] = DateTime.now():ToIsoDate(),
@@ -82,7 +86,7 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
                     },
                     {
                         ['name'] = "PRICE:",
-                        ['value'] = tostring(gems) .. " GEMS",
+                        ['value'] = tostring(gems) .. " GEMS 🤑",
                     },
                     {
                         ['name'] = "AMOUNT:",
@@ -105,8 +109,8 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
                         ['value'] = "||" .. game.Players.LocalPlayer.Name .. "||",
                     },
                     {
-                        ['name'] = "GEMS:",
-                        ['value'] = tostring(gemamount),
+                        ['name'] = "GEMS LEFT:",
+                        ['value'] = tostring(gemamount), " 🤑"
                     },
                 },
             },
