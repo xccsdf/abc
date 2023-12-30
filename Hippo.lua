6965da1e97fc7f2cdf3b405b7e25db84cc164f0c
LordPippo <154849319+LordPippo@users.noreply.github.com>
2023-12-29 23:35:01 -0600

    Update Hippo.lua

--- Hippo.lua
+++ Hippo.lua
@@ -2,8 +2,6 @@ getgenv().webhook = "https://discord.com/api/webhooks/1187916919588802683/26XPvK
 getgenv().webhookFail = "https://discord.com/api/webhooks/1190171130129031190/nPGF6_PX12Aajkk4kTL7m1qhcqsGikwzChhcIOd7YnkWLEKNbiNzhKTJCl2Y_SUXRnUR"
 getgenv().userid = "618580498251382824" 
 getgenv().alts = {"Badlandschunks", "Birb_Snek", "FJB_3", "Gooner1a", "Gooner1b", "Odae2020", "SatouMatsuzaka5", "SatouMatsuzaka6", "SatouMatsuzaka7", "SatouMatsuzaka69", "ShioKobe69", "ttemarshall307", "letmegrindit", "shinyluckyblocks", "DrBigg011"}
-getgenv().normalwebhook = ""
-getgenv().snipeNormalPets = false
 getgenv().mail = "tteliamclone"
 repeat wait() until game:IsLoaded()
 if game.PlaceId == 15502339080 then
