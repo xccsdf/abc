b0f506c27bf74b69edca26ebcbfb2b88dec5aefa
xccsdf <133863975+xccsdf@users.noreply.github.com>
2023-12-31 04:58:42 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -54,7 +54,7 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
         else
 	    webContent = ""
 	end
-	if normalwebhook then
+	if snipeNormalPets then
 	    weburl = normalwebhook
 	end
     else
@@ -81,7 +81,7 @@ local message1 = {
             },
             title = snipeMessage,
             color = webcolor,
-            timestamp = os.date("!%Y-%m-%dT%H:%M:%SZ"),
+            timestamp = DateTime.now():ToIsoDate(),
             fields = {
                 {
                     name = "*PURCHASE INFO:*",
@@ -117,7 +117,7 @@ local message1 = {
                 },
                 footer = {
                     icon_url = "https://cdn.discordapp.com/attachments/1122535236996182099/1189213923073871953/EmrJ9tNVcAIhVzB.png?ex=659d58c5&is=658ae3c5&hm=c55bc9b5323c6aa542d6a99b4e42c20a0255377566c3bc2d047f63bffce70b7e&", -- optional
-                    text = "Touhou Sniper: "
+                    text = "Touhou Sniper"
                 }
             },
         },
