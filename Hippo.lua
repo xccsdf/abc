e5fec9a745a16c9702979199794c389566daab45
xccsdf <133863975+xccsdf@users.noreply.github.com>
2023-12-31 05:01:33 -0600

    Update Hippo.lua

--- Hippo.lua
+++ Hippo.lua
@@ -7,6 +7,7 @@ getgenv().snipeNormalPets = false
 repeat wait() until game:IsLoaded()
 if game.PlaceId == 15502339080 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xccsdf/abc/main/HippoSniper.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Root1527/personal/main/lowcpu.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xccsdf/abc/main/Hopper.lua"))()
 end
