331fa451e6ede8edc6ea7cce870319f921e15734
xccsdf <133863975+xccsdf@users.noreply.github.com>
2024-01-01 17:32:39 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -1,4 +1,4 @@
-local osclock = os.clock()
+	local osclock = os.clock()
 repeat task.wait() until game:IsLoaded()
 
 setfpscap(30)
@@ -156,40 +156,50 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
 
     local price = gems / amount
 
-if type.exclusiveLevel and price <= 10000 and item ~= "Banana" and item ~= "Coin" then
-    local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
-    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
-elseif item == "Titanic Christmas Present" and price <= 25000 then
-    local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
-    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
-elseif string.find(item, "Exclusive") and price <= 25000 then
-    local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
-    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
-elseif item == "Crystal Key" and gems <= 15000 then
-    game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-    local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
-    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
-elseif item == "Crystal Key Lower Half" and gems <= 5000 then
-    game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-    local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
-    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
-elseif item == "Crystal Key Upper Half" and gems <= 10000 then
-    game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-    local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
-    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
-elseif item == "Spinny Wheel Ticket" and gems <= 5000 then
-    game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-    local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
-    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
-elseif item == "Booth Slot Voucher" and gems <= 25000 then
-    game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-    local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
-    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
-elseif type.huge and price <= 1000000 then
-    local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
-    if boughtPet == true then
-        ping = true
+    if type.exclusiveLevel and price <= 10000 and item ~= "Banana" and item ~= "Coin" then
+        local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
+    elseif item == "Titanic Christmas Present" and price <= 25000 then
+        local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
+	processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
+    elseif string.find(item, "Exclusive") and price <= 25000 then
+        local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
+	processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
+    elseif item == "Crystal Key" and gems <= 15000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif item == "Crystal Key Lower Half" and gems <= 5000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+	  processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif item == "Crystal Key Upper Half" and gems <= 10000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+	  processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif item == "Spinny Wheel Ticket" and gems <= 5000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+	  processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif item == "Booth Slot Voucher" and gems <= 25000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+	  processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif class == "Charm" and gems <= 10000 then
+        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+	  processListingInfo(uid, gems, item, version, shiny, amount, username)
+    elseif type.huge and price <= 1000000 then
+        local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
+        if boughtPet == true then
+            ping = true
+	end
+        processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)  
+    elseif type.titanic and price <= 10000000 then
+        local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
+        if boughtPet == true then
+	    ping = true
+	end
+        processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
+    elseif gems == 1 and snipeNormalPets == true then
+	local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)  
     end
+end
 
 Booths_Broadcast.OnClientEvent:Connect(function(username, message)
     local playerIDSuccess, playerError = pcall(function()
@@ -263,7 +273,7 @@ local function jumpToServerIfHighPingAndPlayerLimit()
         end
     end
 
-    local pingThreshold = 250 -- Change this value to your desired ping threshold
+    local pingThreshold = 300 -- Change this value to your desired ping threshold
     local playerLimitThreshold = 40 -- Change this value to your desired player limit threshold
 
     for _, server in ipairs(servers) do
