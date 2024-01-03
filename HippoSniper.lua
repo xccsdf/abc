cf69fd242a26bb91f4c80733f36396320304f8df
xccsdf <133863975+xccsdf@users.noreply.github.com>
2024-01-03 03:18:47 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -153,7 +153,6 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
     local price = gems / amount
 
     wait(3)
-end
      
     -- Pets and Eggs
     if type.huge and price <= 1000000 then
