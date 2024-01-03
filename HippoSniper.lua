2da19419b114fdba413483d69447cc8305fe0f8b
xccsdf <133863975+xccsdf@users.noreply.github.com>
2024-01-03 06:47:30 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -119,7 +119,7 @@ local message1 = {
                 },
             },
             footer = {
-                text = "Touhou Sniper"
+                text = "Touhou Sniper",
                 icon_url = "https://cdn.discordapp.com/attachments/1122535236996182099/1189213923073871953/EmrJ9tNVcAIhVzB.png?ex=659d58c5&is=658ae3c5&hm=c55bc9b5323c6aa542d6a99b4e42c20a0255377566c3bc2d047f63bffce70b7e&", -- optional
             }
         },
