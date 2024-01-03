2034713697108ef6bb417df6e46a387cce457891
xccsdf <133863975+xccsdf@users.noreply.github.com>
2024-01-03 06:23:50 -0600

    Update Hippo.lua

--- Hippo.lua
+++ Hippo.lua
@@ -1,7 +1,7 @@
 getgenv().webhook = "https://discord.com/api/webhooks/1187916919588802683/26XPvKERAu1M-kbxLQCnv1HVBdG3PfnDVm8VnHFtWPoD6neg3kunMXE8NdcJyCmDFvDM"
 getgenv().webhookFail = "https://discord.com/api/webhooks/1190171130129031190/nPGF6_PX12Aajkk4kTL7m1qhcqsGikwzChhcIOd7YnkWLEKNbiNzhKTJCl2Y_SUXRnUR"
 getgenv().userid = "618580498251382824"
-getgenv().alts = {"Badlandschunks", "Birb_Snek", "FJB_3", "Gooner1a", "Gooner1b", "Odae2020", "SatouMatsuzaka5", "SatouMatsuzaka6", "SatouMatsuzaka7", "SatouMatsuzaka69", "ShioKobe69", "ttemarshall307", "letmegrindit", "shinyluckyblocks", "DrBigg011", "hevalbendkf", "zylovespetsim", "SqueakyPeanuts58", "Alter_Ant", "HABhsis", "prinzemark1020"}
+getgenv().alts = {"Badlandschunks", "Birb_Snek", "FJB_3", "SatouMatsuzaka5", "SatouMatsuzaka6", "SatouMatsuzaka7", "SatouMatsuzaka69", "ShioKobe69"}
 getgenv().normalwebhook = "https://discord.com/api/webhooks/1187916919588802683/26XPvKERAu1M-kbxLQCnv1HVBdG3PfnDVm8VnHFtWPoD6neg3kunMXE8NdcJyCmDFvDM"
 getgenv().snipeNormalPets = false
 repeat wait() until game:IsLoaded()
