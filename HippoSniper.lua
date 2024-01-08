2d3ab14e2cc888bc3016f75f201cc78d7b0a90bd
xccsdf <133863975+xccsdf@users.noreply.github.com>
2024-01-08 16:09:59 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -196,7 +196,7 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
                 elseif type.titanic and unitGems <= 10000000 then
                     coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
                     return
-                elseif type.huge and unitGems <= 1000000 then
+                elseif type.huge and unitGems <= 3000000 then
                     coroutine.wrap(tryPurchase)(uid, gems, item, version, boughtPet, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
                     return
                 end
