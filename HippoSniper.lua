71a7f227b2094ec4c28f241d9e31448f6ada33e6
xccsdf <133863975+xccsdf@users.noreply.github.com>
2024-01-09 01:02:00 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -288,10 +288,10 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
                     return
 
                     -- Potion   
-                elseif item == "The Cocktail" and gems <= 50000 then
+                elseif item == "The Cocktail" and unitGems <= 50000 then
                     coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
                     return
-                elseif string.find(item, "Potion VIII") and price <= 50000 and item == "Diamonds Potion VI" then
+                elseif string.find(item, "Potion VIII") and unitGems <= 50000 and item == "Diamonds Potion VI" then
                     coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
                     return
 
