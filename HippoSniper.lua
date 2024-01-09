26e26df0e36453bf0c0cf7680e984ff5d1cae947
xccsdf <133863975+xccsdf@users.noreply.github.com>
2024-01-09 01:16:26 -0600

    Update HippoSniper.lua

--- HippoSniper.lua
+++ HippoSniper.lua
@@ -127,7 +127,8 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
                     },
                     {
 			name = "🚀 PING:",
-			value = Players.LocalPlayer:GetNetworkPing() .. "ms",       
+			value = Players.LocalPlayer:GetNetworkPing() .. "ms",  
+                    },   
 		},
                 footer = {
                     icon_url = "https://cdn.discordapp.com/attachments/1122535236996182099/1189213923073871953/EmrJ9tNVcAIhVzB.png?ex=659d58c5&is=658ae3c5&hm=c55bc9b5323c6aa542d6a99b4e42c20a0255377566c3bc2d047f63bffce70b7e&", -- optional
